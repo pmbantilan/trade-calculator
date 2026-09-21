7f4bcefa81dfe1acb725e7234eb0e6333db2c149
pmbantilan <pmbantilan@yahoo.com>
2026-09-21 17:58:25 +0800

    Upgrade service worker to version 6
    Updated service worker to version 6 with improved cache management.

--- sw.js
+++ sw.js
@@ -1,4 +1,25 @@
-const CACHE="trade-calculator-v5";const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="trade-calculator-v6";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate",e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(
+        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      )
+    ).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
