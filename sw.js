77ef2bb9c32815eba4b02f0023c3f884ff0cdbb3
pmbantilan <pmbantilan@yahoo.com>
2026-09-21 17:49:55 +0800

    Update service worker cache version to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="trade-calculator-v2";const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+const CACHE="trade-calculator-v5";const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
